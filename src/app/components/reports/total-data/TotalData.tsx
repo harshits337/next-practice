825010bc63f333cb48b0e95e25bb0f47a8c2eaa9
Harshit <harshit.sharma@inmobi.com>
2025-01-18 22:11:06 +0530

    Changes

---
+++ src/app/components/reports/total-data/TotalData.tsx
@@ -0,0 +1,9 @@
+const TotalData = () => {
+    return (
+        <div>
+        <h1>Total Data</h1>
+        </div>
+    );
+}
+
+export default TotalData;
---
+++ src/app/dashboard/reports/page.tsx
@@ -0,0 +1,11 @@
+import TotalData from "@/app/components/reports/total-data/TotalData";
+
+export default async function Page() {
+    return (
+        <div>
+            <h1>Hi Harshit</h1>
+        <h3>You can check your reports over here</h3>
+            <TotalData/>
+        </div>
+    );
+}
