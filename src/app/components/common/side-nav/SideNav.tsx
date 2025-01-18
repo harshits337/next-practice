bf68f86849afd2ca3a7b4a5c3d7a1e0938eae4cd
Harshit <harshit.sharma@inmobi.com>
2025-01-18 15:13:42 +0530

    Side Nav setup done

--- src/app/components/common/side-nav/SideNav.tsx
+++ src/app/components/common/side-nav/SideNav.tsx
@@ -1,34 +1,49 @@
 import Link from "next/link";
-import styles from './SideNav.module.css'
+import styles from "./SideNav.module.css";
+import React from "react";
+import { BarChartOutlined, PlusCircleOutlined, PoweroffOutlined } from "@ant-design/icons";
 const SideNav = () => {
-    const links = [
-        {
-            name: 'My Tasks',
-            url: '/dashboard',
-            icon: 'tasks'
-
-        },
-        {
-            name: 'Reports',
-            url: '/reports',
-            icon: 'reports'
-
-        }
-    ]
-    return (
-        <div className={styles.nav}>
-            <img src="/logo.png" alt="logo" width="150"/>
-           {links.map((link,index)=>{
-            return <Link key={index} href={link.url}>
-                <div className={styles.links}>{link.name}</div>
+  const links = [
+    {
+      name: "My Tasks",
+      url: "/dashboard",
+      icon: (
+        <React.Fragment>
+          <PlusCircleOutlined />
+        </React.Fragment>
+      ),
+    },
+    {
+      name: "Reports",
+      url: "/reports",
+      icon: (
+        <React.Fragment>
+          <BarChartOutlined />
+        </React.Fragment>
+      ),
+    },
+  ];
+  return (
+    <div className={styles.nav}>
+      <img src="/logo.png" alt="logo" width="150" />
+      <div className={styles.linkContainer}>
+        {links.map((link, index) => {
+          return (
+            <Link key={index} href={link.url}>
+              <div className={styles.links}>
+                {link.icon}
+                {link.name}
+              </div>
             </Link>
-           })}
+          );
+        })}
+      </div>
 
-           <div className={styles.logout}>
-                <div>Logout</div>
-           </div>
-        </div>
-    );
-}
+      <div className={styles.logout}>
+        <PoweroffOutlined /> <span>Logout</span>
+      </div>
+    </div>
+  );
+};
 
 export default SideNav;
